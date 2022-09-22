4587969e39d79e1a4823635a34d5fb2179a6e92d
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-09-22 14:39:27 +0200

    added a docker-compose file + mapping student class to DB

--- src/main/java/com/manalrzzl/professionalproject/student/Student.java
+++ src/main/java/com/manalrzzl/professionalproject/student/Student.java
@@ -1,5 +1,13 @@
 package com.manalrzzl.professionalproject.student;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import lombok.*;
 
 @ToString
@@ -8,10 +16,27 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+  @Id
+  @SequenceGenerator(
+      name = "student_sequence",
+      sequenceName = "student_sequence",
+      allocationSize = 1
+  )
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "student_sequence")
   private Long id;
   private String name;
   private String email;
+  @Enumerated(EnumType.STRING)
   private Gender gender;
 
+  public Student(String name, String email, Gender gender) {
+    this.name = name;
+    this.email = email;
+    this.gender = gender;
+  }
 }
