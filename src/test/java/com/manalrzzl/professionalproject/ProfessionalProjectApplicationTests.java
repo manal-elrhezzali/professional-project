5ae85fe71c783686c27d1b85a7f706563fee47ed
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-10-05 20:52:04 +0200

    failing test

--- src/test/java/com/manalrzzl/professionalproject/ProfessionalProjectApplicationTests.java
+++ src/test/java/com/manalrzzl/professionalproject/ProfessionalProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.manalrzzl.professionalproject;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class ProfessionalProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("The test failed");
 	}
 
 }
